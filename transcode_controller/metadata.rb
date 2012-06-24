aee9dbc500f5f82f36dee4facdf199173c09bc34
Ryan J. Geyer <me@ryangeyer.com>
2012-06-24 12:39:44 -0700

    Added a create job recipe, and updated the controller gem

--- transcode_controller/metadata.rb
+++ transcode_controller/metadata.rb
@@ -10,6 +10,7 @@ supports "ubuntu"
 depends "rs_utils"
 
 recipe "transcode_controller::install", "Installs the transcode controller gem and any required libs"
+recipe "transcode_controller::do_create_jobs_from_rss", "Executes gio_2012_controller once for each RSS feed that's supplied"
 
 attribute "cloud/google/store/key",
   :display_name => "Google Storage API Key",
@@ -20,3 +21,14 @@ attribute "cloud/google/store/secret",
   :display_name => "Google Storage API Secret",
   :required => "required",
   :recipes => ["transcode_controller::install"]
+
+attribute "transcode/amqp/host",
+  :display_name => "Transcode AMQP Hostname",
+  :required => "required",
+  :recipes => ["transcode_controller::do_create_jobs_from_rss"]
+
+attribute "transcode/rss_sources",
+  :display_name => "Transcoding RSS Sources",
+  :required => "required",
+  :type => "array",
+  :recipes => ["transcode_controller::do_create_jobs_from_rss"]
