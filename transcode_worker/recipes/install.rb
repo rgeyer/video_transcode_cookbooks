40f51bec12081568ae6f3c04026b14e35cb1d33e
Ryan J. Geyer <me@ryangeyer.com>
2012-06-27 00:49:50 -0700

    Don't enable the reconverge

--- transcode_worker/recipes/install.rb
+++ transcode_worker/recipes/install.rb
@@ -51,7 +51,7 @@ cron "Re-run transcode_worker::do_start_workers" do
   minute "*/2"
   user "root"
   command "rs_run_recipe -n transcode_worker::do_start_workers 2>&1 > /var/log/rs_sys_reconverge.log"
-  action :create
+  action :nothing
 end
 
 rs_utils_marker :end
